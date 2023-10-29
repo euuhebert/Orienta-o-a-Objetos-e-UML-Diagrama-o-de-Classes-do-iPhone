9fc8317faef730c99fb2253e20ccb209da281954
euuhebert <hebertsrocha79@gmail.com>
2023-10-28 22:12:53 -0300

    teste

---
+++ src/IPhone.java
@@ -0,0 +1,47 @@
+public class IPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    @Override
+    public void tocar() {
+        // Implementação para tocar música
+    }
+
+    @Override
+    public void pausar() {
+        // Implementação para pausar música
+    }
+
+    @Override
+    public void selecionarMusica() {
+        // Implementação para selecionar uma música
+    }
+
+    @Override
+    public void ligar() {
+        // Implementação para ligar o telefone
+    }
+
+    @Override
+    public void atender() {
+        // Implementação para atender chamadas
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        // Implementação para iniciar correio de voz
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        // Implementação para exibir uma página na internet
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        // Implementação para adicionar uma nova aba no navegador
+    }
+
+    @Override
+    public void atualizarPagina() {
+        // Implementação para atualizar a página no navegador
+    }
+}
